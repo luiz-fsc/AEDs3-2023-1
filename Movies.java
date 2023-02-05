229dcb93fba844ca99d7d8227da1d72a9c5cfdbb
luiz-fsc <luizfs448@gmail.com>
2023-02-05 10:44:54 -0300

    classe movies criada

---
+++ Movies.java
@@ -0,0 +1,20 @@
+import java.time.Year;
+import java.util.List;
+
+public class Movies{
+    int ranking;
+    String title;
+    List<String> genres;
+    String description;
+    String director;
+    List<String> actors;
+    Year year;
+    int runTime;
+    float rating;
+    int votes;
+    float revenue;
+    byte metascore;
+
+    
+
+}
