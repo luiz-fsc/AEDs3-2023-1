5fb376919c42c8e9c91ff850c977dad6e7e92100
luiz-fsc <luizfs448@gmail.com>
2023-02-23 08:21:10 -0300

    split das linhas lidas em campos

--- Projeto-Luiz/MyCSVReader.java
+++ Projeto-Luiz/MyCSVReader.java
@@ -8,14 +8,27 @@ public class MyCSVReader {
     static String line = "";
 
     public static void openArq() throws IOException{
-
+        
         FileReader fr = new FileReader(path);
         BufferedReader br = new BufferedReader(fr);
 
-        br.readLine();
+        br.readLine();//skip da linha de cabeçalho do csv
         line = br.readLine();
         while(line != null){
-            
+            //FALTA TRATAR CADA UM DOS CAMPOS DE ACORDO COM SEUS TIPOS
+            String[] field = line.split(",");
+            String title = field[1];
+            String genre = field[2];
+            String description = field[3];
+            String director = field[4];
+            String actors = field[5];
+            String year = field[6];
+            String runTime = field[7];
+            String rating = field[8];
+            String votes = field[9];
+            String revenue = field[10];
+            String metaScore = field[11];
+
         }
 
         br.close();
