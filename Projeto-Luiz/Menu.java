f39a5213db96f6da1514f72f6d9a127270065ebb
luiz-fsc <luizfs448@gmail.com>
2023-02-25 08:07:07 -0300

    update 25-02

--- Projeto-Luiz/Menu.java
+++ Projeto-Luiz/Menu.java
@@ -1,9 +1,21 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        //Setando o arquivo principal de armazenamento de dados
+        final String arqdb = "filmes.db";
+
+        //Organização inicial para funcionamento do programa
+        //====================================================================================
+        RandomAccessFile raf = new RandomAccessFile(arqdb, "rw");
+        raf.writeInt(0);//escrevo zero para auxiliar na geração dos proximos ids das contas
         Scanner sc = new Scanner(System.in);
         int option;
+        //====================================================================================
+
 
         System.out.println("Selecione uma das opcoes abaixo (digite apenas o numero correspondente): \n1 - Carregar base de dados \n2 - Ler um registro \n3 - Atualizar um registro \n4 - Deletar um registro \n0 - sair");
         
@@ -14,7 +26,11 @@ public class Menu {
             System.out.println("Obrigado por usar o sistema!");
                 break;
             case 1:
-                //criar base de dados
+                try {
+                    MyCSVReader.openArq();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case 2: 
                 //ler um registro
@@ -27,5 +43,7 @@ public class Menu {
                 break;
         }
         sc.close();
+        raf.close();
+
     }
 }
--- Projeto-Luiz/MyCSVReader.java
+++ Projeto-Luiz/MyCSVReader.java
@@ -1,40 +1,50 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class MyCSVReader {
 
     static String path = "C:\\DataBases\\IMDB-Movie-Data.csv";
-    static String line = "";
-
+    
     public static void openArq() throws IOException{
-        
+
+
         FileReader fr = new FileReader(path);
         BufferedReader br = new BufferedReader(fr);
 
+        String line = "";
+        
         br.readLine();//skip da linha de cabeçalho do csv
-        line = br.readLine();
+        line = br.readLine();//Primeira linha do arquivo csv que realmente contem dados uteis
+        
         while(line != null){
-            //FALTA TRATAR CADA UM DOS CAMPOS DE ACORDO COM SEUS TIPOS
-            String[] field = line.split(",");
-            String title = field[1];
-            String genre = field[2];
-            String description = field[3];
-            String director = field[4];
-            String actors = field[5];
-            String year = field[6];
-            String runTime = field[7];
-            String rating = field[8];
-            String votes = field[9];
-            String revenue = field[10];
-            String metaScore = field[11];
+            
+            String[] field = line.split("\"");
+            String title = field[0].split(",")[1];
+            List<String> genres = Arrays.asList(field[1].split(","));
 
+
+            String description = field[2].split(",")[1];
+            String director = field[2].split(",")[2]; 
+            List<String> actors = Arrays.asList(field[3].split(","));
+
+            String[] values = field[4].split(",");
+            int year = Integer.parseInt(values[1]);
+            int runTime = Integer.parseInt(values[2]);
+            float rating = Float.parseFloat(values[3]);
+            int votes = Integer.parseInt(values[4]);
+            float revenue = Float.parseFloat(values[5]);
+            byte metaScore = Byte.parseByte(values[6]);
+
+            Movies aux = new Movies(-1, title, genres, description, director, actors, year, runTime, rating, votes ,revenue, metaScore);
+
+
+            line = br.readLine();
         }
 
         br.close();
-
+        fr.close();
     }
-     
-    
-    
 }
