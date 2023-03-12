7723a3b7615191fe5a934a0623d5dc8e6ffbb261
luiz-fsc <luizfs448@gmail.com>
2023-03-12 10:57:34 -0300

    intercalacao

---
+++ java_projeto/src/Intercalacao.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+public class Intercalacao {
+
+    public void distribuicao(int bloco, int caminhos, String path) throws FileNotFoundException{
+
+        RandomAccessFile raf = new RandomAccessFile(path, "rw");
+        
+
+    }
+
+}
