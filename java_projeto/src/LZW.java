f2243ce55052a25dbd27a5369b53fb23a102b521
luiz-fsc <luizfs448@gmail.com>
2023-05-21 12:45:18 -0300

    descompressão iniciada

--- java_projeto/src/LZW.java
+++ java_projeto/src/LZW.java
@@ -1,22 +1,25 @@
 import java.util.*;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class LZW {
     
-    public static List<Integer> comprimir(String original) {
-        Map<String, Integer> dicionario = new HashMap<String, Integer>();
-
-        //Inicializar o dicionário (com símbolos básicos)
+    public static void comprimir(String original) {
+        
+        Map<String, Integer> dicionario = new HashMap<>();
         for (int i = 0; i < 256; i++) {
-            dicionario.put("" + (char)i, i);
+            dicionario.put(String.valueOf((char) i), i);
         }
         
         //em "atual" será armazenada a maior string existente no dicionário
         String atual = "";
         //em "resultado" será armazenado a lista de códigos da saida do algoritmo
         List<Integer> resultado = new ArrayList<Integer>();
+        //tamDicionario guarda o tamanho do dicionário
+        int tamDicionário = 256;
 
 
         for (char c : original.toCharArray()) {
@@ -29,7 +32,8 @@ public class LZW {
                 //adiciono ao resultado o código da maior string achada no dicionário e armazenada em "atual"
                 resultado.add(dicionario.get(atual));
                 //adiciono ao dicionário a maior string achada no dicionário + o char seguinte
-                dicionario.put(next, dicionario.size());
+                dicionario.put(next, tamDicionário);
+                tamDicionário++;
                 //atualizo "atual" para armazenar o próximo char fora da ultima string achada no dicionário e continuar o algoritmo
                 atual = "" + c;
             }
@@ -39,26 +43,56 @@ public class LZW {
         if (!atual.isEmpty()) {
             resultado.add(dicionario.get(atual));
         }
-        return resultado;
-    }
 
-    public static void main(String[] args) {
-        String original = new String();
-
-        try{
-            original = lerArquivoDB();
-        }catch (Exception e){
+        //escrevendo os codigos de saida do algoritmo em um arquivo
+        try (DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("java_projeto\\comprimidos\\arqComprimido.bin"))) {
+            for (Integer cod : resultado) { //para todo cod existente na lista de inteiros resultado
+                outputStream.writeInt(cod); //escrever no arquivo em binario
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Compactação finalizada!");
 
-        List<Integer> str_comprimida = comprimir(original);
-        System.out.println("str_comprimida: " + str_comprimida);
     }
 
+    public static void descomprimir(List<Integer> compressed){
+        String path = "java_projeto\\comprimidos\\arqComprimido.bin";
 
-    public static String lerArquivoDB() throws IOException {
+        Map<Integer, String> dicionario = new HashMap<>();
+        for (int i = 0; i < 256; i++) {
+            dicionario.put(i, String.valueOf((char) i));
+        }
 
-        File arquivo = new File("dados\\filmes\\arquivo.db");
+        int tamDicionario = 256;
+        String current = dicionario.get(compressed.get(0));
+        StringBuilder resultado = new StringBuilder(current);
+
+        
+        for (int i = 1; i < compressed.size(); i++) {
+            int code = compressed.get(i);
+
+            String entry;
+            if (dicionario.containsKey(code)) {
+                entry = dicionario.get(code);
+            } else if (code == tamDicionario) {
+                entry = current + current.charAt(0);
+            } else {
+                throw new IllegalArgumentException("Invalid compressed code: " + code);
+            }
+
+            resultado.append(entry);
+            dicionario.put(tamDicionario, current + entry.charAt(0));
+            tamDicionario++;
+            current = entry;
+        }
+
+        System.out.println(resultado.toString());
+    }
+
+    public static String lerArquivoDB() throws IOException { 
+
+        File arquivo = new File("java_projeto\\dados\\filmes\\arquivo.db");
         
         StringBuilder sb = new StringBuilder();
         
@@ -75,4 +109,16 @@ public class LZW {
         return sb.toString();
     }
 
+    public static void main(String[] args) {
+        String original = new String();
+        
+        //armazenamos em "original" o conteudo em formato de string de todo o arquivo de dados
+        try{
+            original = lerArquivoDB();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //System.out.println(original);
+        comprimir(original);
+    }
 }
