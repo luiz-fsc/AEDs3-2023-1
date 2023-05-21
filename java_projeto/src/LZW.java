8a9ad69eaabf2d10ed542860bbdb95fb9468c595
luiz-fsc <luizfs448@gmail.com>
2023-05-21 08:41:56 -0300

    Compressão comentada

--- java_projeto/src/LZW.java
+++ java_projeto/src/LZW.java
@@ -6,27 +6,36 @@ import java.io.IOException;
 public class LZW {
     
     public static List<Integer> comprimir(String original) {
-        Map<String, Integer> dicionario = new HashMap();
-        
+        Map<String, Integer> dicionario = new HashMap<String, Integer>();
+
+        //Inicializar o dicionário (com símbolos básicos)
         for (int i = 0; i < 256; i++) {
             dicionario.put("" + (char)i, i);
         }
         
+        //em "atual" será armazenada a maior string existente no dicionário
         String atual = "";
-        List<Integer> resultado = new ArrayList();
+        //em "resultado" será armazenado a lista de códigos da saida do algoritmo
+        List<Integer> resultado = new ArrayList<Integer>();
 
 
         for (char c : original.toCharArray()) {
+            //em "next" será armazenada a maior string possível existente no dicionário + o caractere seguinte(o que causou a falha na busca pelo dicionário)
             String next = atual + c;
             if (dicionario.containsKey(next)) {
+                //se a string for achada no dicionário, atualizar atual para continuar a busca da maior string possível
                 atual = next;
             } else {
+                //adiciono ao resultado o código da maior string achada no dicionário e armazenada em "atual"
                 resultado.add(dicionario.get(atual));
+                //adiciono ao dicionário a maior string achada no dicionário + o char seguinte
                 dicionario.put(next, dicionario.size());
+                //atualizo "atual" para armazenar o próximo char fora da ultima string achada no dicionário e continuar o algoritmo
                 atual = "" + c;
             }
         }
         
+        //se em atual ainda houver alguma string, adiciono o codigo dela à resultado
         if (!atual.isEmpty()) {
             resultado.add(dicionario.get(atual));
         }
@@ -34,14 +43,11 @@ public class LZW {
     }
 
     public static void main(String[] args) {
-        LZW lzw = new LZW();
         String original = new String();
 
         try{
-
-            original = lzw.lerArquivoDB();
-
-          }catch(Exception e){
+            original = lerArquivoDB();
+        }catch (Exception e){
             e.printStackTrace();
         }
 
